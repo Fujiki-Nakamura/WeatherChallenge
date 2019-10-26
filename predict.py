4df7b25bfcab8bdf838b74768ed25ca0860d07b5
fnakamura <fujiki.nakamura@gmail.com>
2019-10-26 11:51:27 +0900

    [refactor] predict.py. validate w/ X_2018_50x96x{168,672}x{128,512}x1.hkl

--- predict.py
+++ predict.py
@@ -4,7 +4,6 @@ Calculates mean-squared error and plots predictions.
 '''
 import datetime as dt
 import os
-import sys
 
 import hickle as hkl
 import numpy as np
@@ -24,22 +23,19 @@ n_plot = 50
 batch_size = 10
 nt = 48
 extrap_start_time = 24
-last_n_timestep = 24
 input_h, input_w, input_c = 168, 128, 1
+input_ts = 24
 TARGET_TS = 24
 sample_submit = '../inputs/sample_submit.csv'
 
 ID = dt.datetime.now().strftime('%Y%m%d%H%M%S')
 EXP_ID = '20191022064146'
 DATA_DIR = '../inputs/hkl/'
-WEIGHTS_DIR = './logs/{}/'.format(EXP_ID)
-RESULTS_SAVE_DIR = './logs/{}/'.format(EXP_ID)
-INPUT_PATH = os.path.join(DATA_DIR, 'X_test_168x128.hkl')
-TARGET_PATH = os.path.join(DATA_DIR, 'y_valid_168x128.hkl')
-is_making_submission = True
-PLOT = False
-valid = False
-test = True
+WEIGHTS_DIR = '../logs.prednet/{}/'.format(EXP_ID)
+RESULTS_SAVE_DIR = '../logs.prednet/{}/'.format(EXP_ID)
+INPUT_PATH = os.path.join(DATA_DIR, 'X_2018_50x96x168x128x1.hkl')
+TARGET_PATH = os.path.join(DATA_DIR, 'X_2018_50x96x672x512x1.hkl')
+is_making_submission = False
 
 
 def resize(X):
@@ -78,25 +74,23 @@ def main():
     predictions = test_prednet(inputs)
     test_model = Model(inputs=inputs, outputs=predictions)
 
-    X_test = hkl.load(INPUT_PATH)
-    zeros = np.zeros((len(X_test), nt - last_n_timestep, input_h, input_w, input_c))
-    y_test = hkl.load(TARGET_PATH) if valid else zeros
+    X_test = hkl.load(INPUT_PATH)[:, :nt]
+    y_test = hkl.load(TARGET_PATH)[:, :nt][:, input_ts:]
+    # zeros = np.zeros((len(X_test), nt - input_ts, input_h, input_w, input_c))
     X_test_inp = X_test / 255.
-    if last_n_timestep > 0:
-        X_test_inp = X_test_inp[:, -last_n_timestep:]
-        X_test_inp = np.concatenate([X_test_inp, zeros], axis=1)
     if data_format == 'channels_first':
         X_test_inp = X_test_inp.transpose((0, 1, 4, 2, 3))
-    assert np.sum(X_test_inp[:, -last_n_timestep:]) == 0.
+    # assert np.sum(X_test_inp[:, -last_n_timestep:]) == 0.
     X_hat = test_model.predict(X_test_inp, batch_size)
+    assert X_hat.shape[1] == nt
     if data_format == 'channels_first':
         X_test_inp = np.transpose(X_test_inp, (0, 1, 3, 4, 2))
         X_hat = np.transpose(X_hat, (0, 1, 3, 4, 2))
 
-    X_hat = (255. - np.round(X_hat * 255)).clip(0, 255)
+    X_hat = np.round(X_hat * 255).clip(0, 255)
     if is_making_submission:
         X_hat = resize(X_hat)
-        X_hat = X_hat[:, -last_n_timestep:]
+        X_hat = X_hat[:, input_ts:]
         assert X_hat.shape[1] == TARGET_TS
         # make submission
         indices = [i for i in range(TARGET_TS) if i % 6 == 5]
@@ -109,24 +103,18 @@ def main():
         df.to_csv(path, index=False, header=False)
         print('Submission saved at {}'.format(path))
     else:
-        if not PLOT:
-            X_hat = resize(X_hat)
-            # MAE
-            MAE = np.mean(np.abs(y_test - X_hat[:, -last_n_timestep:]))
-            MAE_eval = np.mean(np.abs(
-                crop_eval_area(y_test) - crop_eval_area(X_hat[:, -last_n_timestep:])))
-            print('MAE/Valid {:.4f} MAE-eval/Valid {:.4f}'.format(MAE, MAE_eval))
-            sys.exit(0)
+        # MAE
+        MAE = np.mean(np.abs(
+            crop_eval_area(y_test) - crop_eval_area(resize(X_hat[:, input_ts:]))))
+        print('MAE/Valid {:.4f}'.format(MAE))
 
-        X_test = np.concatenate([X_test[:, -last_n_timestep:], y_test], axis=1)
         # Plot some predictions
         aspect_ratio = float(X_hat.shape[2]) / X_hat.shape[3]
         plt.figure(figsize=(nt, 2*aspect_ratio))
         gs = gridspec.GridSpec(2, nt)
         gs.update(wspace=0., hspace=0.)
-        split = 'valid' if valid else 'test'
         plot_save_dir = os.path.join(
-            RESULTS_SAVE_DIR, 'plots_{}_ext_{}-{}/'.format(split, nt, extrap_start_time))
+            RESULTS_SAVE_DIR, 'plots_{}ts-{}ext/'.format(nt, extrap_start_time))
         if not os.path.exists(plot_save_dir):
             os.makedirs(plot_save_dir)
             os.chmod(plot_save_dir, 0o0777)
