1d1fecfbb7d25829822886088467f8fd397fb2b2
fnakamura <fujiki.nakamura@gmail.com>
2019-10-27 19:05:38 +0900

    20191027095300

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main(args):
     f = 4
     args.input_h, args.input_w = int(672 / f), int(512 / f)
     args.ts, args.input_ts, args.target_ts = 48, 24, 24
-    args.output_ts = 12
+    args.output_ts = 24
     args.model = 'encdec_02'
-    args.hidden_dims = [16, 16, ]
+    args.hidden_dims = [16, 16, 16, 16, ]
     args.n_layers = len(args.hidden_dims)
     args.batch_size = 8
     args.optim_str = 'RAdam/lr=0.001/betas=(0.9, 0.999)/weight_decay=0'
