e29896147bed1c41ae85a0b1d97545200dfddfb1
fnakamura <fujiki.nakamura@gmail.com>
2019-10-31 12:17:55 +0900

    20191031031631

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from experiment import run
 
 
 def main(args):
-    f = 4
+    f = 8
     args.input_h, args.input_w = int(672 / f), int(512 / f)
-    args.ts, args.input_ts, args.target_ts = 48, 24, 24
-    args.output_ts = 24
-    args.is_training_with_2018 = True
+    args.ts, args.input_ts, args.output_ts, args.target_ts = 24, 12, 12, 12
+    args.is_training_with_2018 = False
     args.model = 'encdec_02'
     args.hidden_dims = [16, 16, ]
     args.n_layers = len(args.hidden_dims)
-    args.batch_size = 16
+    args.batch_size = 64
     args.optim_str = 'RAdam/lr=0.001/betas=(0.9, 0.999)/weight_decay=0'
     args.teacher_forcing_ratio = -1.
     run(args)
